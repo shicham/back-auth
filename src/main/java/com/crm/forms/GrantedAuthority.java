ee84cee27c25d3213ed60f6afcae4d082705f7ba
shicham <sass.hicham@gmail.com>
2020-12-24 00:54:35 +0100

    Update GrantedAuthority.java

--- src/main/java/com/crm/forms/GrantedAuthority.java
+++ src/main/java/com/crm/forms/GrantedAuthority.java
@@ -8,6 +8,13 @@ import lombok.Data;
 public class GrantedAuthority {
 
     private String authority;
+    
+    public GrantedAuthority(){
+    }
+    
+    public GrantedAuthority(String authority){
+        this.authority=authority;
+    }
 
     
 

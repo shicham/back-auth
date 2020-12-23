fb344cf339b37ac9ab256c3ac8ff280d011e86f2
shicham <sass.hicham@gmail.com>
2020-12-24 00:47:11 +0100

    Update AuthServer.java

--- src/main/java/com/crm/AuthServer.java
+++ src/main/java/com/crm/AuthServer.java
@@ -5,8 +5,6 @@ import com.crm.commun.configs.ServerApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient

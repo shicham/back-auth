df12d255ead3c6ee590c263aa56efc385a308402
shicham <sass.hicham@gmail.com>
2020-12-24 01:11:11 +0100

    Update AuthServer.java

--- src/main/java/com/crm/AuthServer.java
+++ src/main/java/com/crm/AuthServer.java
@@ -1,13 +1,19 @@
 package com.crm;
 
 import com.crm.commun.configs.ServerApplication;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@EnableZuulProxy
 @EnableDiscoveryClient
+@SpringBootApplication
+@RestController
 public class AuthServer  extends ServerApplication{
 
 	public static void main(String[] args) {
